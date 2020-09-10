a278885f2580ba8af3b75f174d58cc220e9c8811
Akkshat Nawani <70836008+Inawan@users.noreply.github.com>
2020-09-10 23:17:13 +0530

    Classes
    Learning about classes and how they work

---
+++ Bank.java
@@ -0,0 +1,57 @@
+package com.project;
+
+public class Bank {
+    private String accountNumber ;
+    private double balance;
+    private String customerName ;
+    private String email ;
+    private String phoneNumber;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance( double balance){
+        this.balance = balance ;
+    }
+    public String getCustomerName(){
+        return customerName;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber ;
+    }
+
+
+    public void deposit(double fundToDeposit){
+        balance += fundToDeposit;
+    }
+
+    public void withdraw( double withdrawAmount){
+        if ( withdrawAmount > balance){
+            System.out.println("Insufficient Balance");
+        }
+        else {
+            balance -= withdrawAmount;
+            System.out.println(" New balance is " + balance);
+        }
+    }
+}
